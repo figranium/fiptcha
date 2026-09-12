3aeeceea3fc40bcebe5669afb4d3e6cf16267bd2
Aser Nasr <asernasr@figranium.dev>
2026-09-12 22:11:07 +0300

    Add 2 GB memory regression test

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -18,4 +18,12 @@ assert.strictEqual(threshold.recall, 1);
 assert.strictEqual(fiptcha.selectModelTier({ totalMb: 4096, availableMb: 1024 }, { CAPTCHA_MODEL_TIER: 'auto' }), 'owlvit');
 assert.strictEqual(fiptcha.selectModelTier({ totalMb: 1024, availableMb: 256 }, { CAPTCHA_MODEL_TIER: 'auto' }), null);
 
+const advertised2GbInMiB = 2_000_000_000 / (1024 * 1024);
+assert(advertised2GbInMiB > 1907 && advertised2GbInMiB < 2048);
+assert.strictEqual(
+  fiptcha.selectModelTier({ totalMb: advertised2GbInMiB, availableMb: 768 }, { CAPTCHA_MODEL_TIER: 'auto' }),
+  'owlvit',
+  'A provider-advertised 2 GB host should qualify even though it is less than 2 GiB'
+);
+
 console.log('fiptcha smoke tests passed');
